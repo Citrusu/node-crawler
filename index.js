f2036b43cb3d89fe24cdceedecbb788116562622
Citrus <zhouyou421@126.com>
2017-03-20 17:55:23 +0800

    init

---
+++ index.js
@@ -0,0 +1,56 @@
+const express = require('express');
+const superagent = require('superagent');
+const cheerio = require('cheerio');
+const app = express();
+
+// 用 superagent 去抓取 https://cnodejs.org/ 的内容
+let getUrl = 'https://cnodejs.org';
+let page = [1, 10];
+let pageParam = '/?tab=all&page=';
+let pageData = [];
+
+//抓取页数
+for(let i = page[0]; i < (page[1] + 1); i++){
+    let url = getUrl + pageParam + i;
+    superagent.get(url).end(function (err, sres) {
+        getData(err, sres, i);
+    });
+}
+function getData(err, sres, index){
+    // 常规的错误处理
+    if (err) {
+        return next(err);
+    }
+    // sres.text 里面存储着网页的 html 内容，将它传给 cheerio.load 之后
+    // 就可以得到一个实现了 jquery 接口的变量，我们习惯性地将它命名为 `$`
+    // 剩下就都是 jquery 的内容了
+    let $ = cheerio.load(sres.text);
+    let pageNode = [];
+    $('#topic_list .topic_title').each(function (idx, element) {
+        let $element = $(element);
+        pageNode.push({
+            title: $element.attr('title'),
+            url: $element.attr('href')
+        });
+    });
+    pageData[index] = pageNode;
+}
+
+app.get('/', function (req, res, next) {
+
+    //展示内容
+    let html = '';
+    pageData.forEach((n, i) => {
+        n.forEach((m, j) => {
+            html += `<h3> ${ n[j].title } </h3>`;
+            html += `<a href="${ getUrl + n[j].url }"> 查看原文章 </a>`;
+        });
+    });
+
+    res.send(html);
+    next();
+});
+
+app.listen(3000, () => {
+   console.log('listening at port 3000');
+});
